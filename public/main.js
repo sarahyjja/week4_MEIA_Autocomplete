0f487d388246c1922ebc93dfb26e517e2489e2ab
reubengt <reubengeorgethomas@gmail.com>
2019-07-25 11:48:06 +0100

    add search input and event listener

--- public/main.js
+++ public/main.js
@@ -1,1 +1,19 @@
-console.log("hello les boubou!")
+const searchBox=document.querySelector('input[name="search-box"]');
+console.log(searchBox);
+searchBox.addEventListener('input', (e) => {
+  let searchTerm = e.target.value;
+  console.log(searchTerm+".");
+  let xhr=new XMLHttpRequest();
+  //example search endpoint : /search?q=fluffyunicorn
+  let searchUrl = `/search?q=${encodeURIComponent(searchTerm)}`
+  console.log(searchUrl);
+  xhr.onreadystatechange = () => {
+    if(xhr.readyState == 4 && xhr.status == 200)
+    {
+      responseArray=JSON.parse(xhr.responseText);
+      console.log(responseArray);
+    }
+  };
+  xhr.open ('GET', searchUrl, true)
+  xhr.send()
+})
