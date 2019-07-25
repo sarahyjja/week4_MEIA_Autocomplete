2357319902ec3b824b2a1c9b8af7d1c55fcb49b4
reubengt <reubengeorgethomas@gmail.com>
2019-07-25 12:41:15 +0100

    add basic highlighting for suggestions list

--- public/main.js
+++ public/main.js
@@ -1,4 +1,7 @@
 const searchBox = document.querySelector('input[name="search-box"]');
+let suggestionsArray=[];
+//counter to store position of highlighted item in list
+let listNavCounter = -1;
 //focus on searchBox by default
 searchBox.focus();
 console.log({ searchBox });
@@ -13,29 +16,37 @@ const changeValue = () => {
   console.log(searchUrl);
   xhr.onreadystatechange = () => {
     if (xhr.readyState == 4 && xhr.status == 200) {
-      responseArray = JSON.parse(xhr.responseText);
-      console.log(responseArray);
-      populateSuggestionBox(responseArray);
+      //populate suggestionsArray with parsed JSON response
+      suggestionsArray = JSON.parse(xhr.responseText);
+      console.log(suggestionsArray);
+      //fill DOM with li elements created from suggestionsArray
+      populateSuggestionBox();
     }
   };
   xhr.open("GET", searchUrl, true);
   xhr.send();
-};  
+};
+//when there is an input, change the value of searchTerm and make a new request inside changeValue
 searchBox.addEventListener("input", changeValue);
-
-const populateSuggestionBox = suggestionsArray => {
+//create li elements for each suggestion
+const populateSuggestionBox = () => {
   const UlElement = document.querySelector(".suggestions-box");
   UlElement.innerHTML = "";
-  suggestionsArray.forEach(suggestion => {
+  suggestionsArray.forEach((suggestion, i) => {
     const liElement = document.createElement("li");
     liElement.classList.add("suggestion-item");
     liElement.textContent = suggestion;
+    //adds highlight class to the element at the index decided by listNavCounter
+    //this index changes when arrow keys are used for navigating and the UL is rerendered each time
+    if(i===listNavCounter){
+      liElement.classList.add('highlighted')
+    }
     UlElement.appendChild(liElement);
     liElement.addEventListener("click", chooseSuggestion);
   });
 };
 
-// event handler for choosing from suggestions
+// event handler for choosing a suggestion and updating value when clicked
 const chooseSuggestion = event => {
   const value = event.target.textContent;
   searchBox.value = value;
@@ -43,3 +54,14 @@ const chooseSuggestion = event => {
   changeValue();
   console.log(value);
 };
+
+//callback to handle navigation keypresses(arrow keys).
+//updates listNavCounter and calls populateSuggestionBox function to rerender suggestions list.
+const navigateList= (e) =>{
+  if(e.keyCode==40){
+    listNavCounter+=1;
+    populateSuggestionBox();
+  }
+}
+//eventlistener for navigating search queries with keyboard
+searchBox.addEventListener('keydown', navigateList)
